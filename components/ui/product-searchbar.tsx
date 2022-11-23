28eafced9441d784127632ff249738a4fc7610f0
Dwight McLeish Jr <dmcleishjr@gmail.com>
2022-11-23 06:30:59 -0500

    chore: adjusted search icon placement

--- components/ui/product-searchbar.tsx
+++ components/ui/product-searchbar.tsx
@@ -3,6 +3,7 @@ import { MdSearch } from 'react-icons/md';
 export default function ProductSearchBar() {
 	return (
 		<form className='flex items-center gap-2'>
+			<MdSearch className='text-white text-3xl' />
 			<input type='text' placeholder='Search..' name='search' />
 		</form>
 	);
